1e675a84137797b6558ff291dc05bfb0419c4d03
x_a1r_x <mirov.asya@yandex.ru>
2023-03-18 00:20:45 +0300

    Add project files.

---
+++ JsonSnakeCaseNamingPolicy/SnakeCaseNamingPolicy.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace JsonSnakeCaseNamingPolicy
+{
+    public class SnakeCaseNamingPolicy : JsonNamingPolicy
+    {
+        public static SnakeCaseNamingPolicy Instance { get; } = new SnakeCaseNamingPolicy();
+        public override string ConvertName(string name)
+        {
+            return string.Concat(name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+    }
+}
